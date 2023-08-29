a7824ec501e071f06decd8d8594b7710ab3ce09c
Nathalia Tenaglia <nathtenaglia@hotmail.com>
2023-08-29 17:38:26 -0300

    read_csv TypeError

---
+++ decision-tree/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.tree import DecisionTreeClassifier
+
+base = pd.read_csv('/sample-data/restaurantev2.csv')
+base
