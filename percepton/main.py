b7a729f963c572763f617aa7534341d6fa996c7d
Nathalia Tenaglia <nathtenaglia@hotmail.com>
2023-11-10 09:35:11 -0300

    feat: perceptron functions

--- percepton/main.py
+++ percepton/main.py
@@ -1,28 +1,52 @@
-from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import random
 
 class Perceptron:
-  def __init__(self, eta=0.01, n_iter=10):
-    self.eta = eta
-    self.n_iter = n_iter
+    def __init__(self, n):
+        self.weights = [random.uniform(-1, 1) for _ in range(n)]
+        self.bias = random.uniform(-1, 1)
 
-  def fit(self, X, y):
-    self.w_ = np.zeros(1 + X.shape[1])
-    self.erros_ = []
+    def predict(self, inputs):
+        weighted_sum = sum(w * x for w, x in zip(self.weights, inputs)) + self.bias
+        return 1 if weighted_sum >= 0 else 0
 
-    for _ in range(self.n_iter):
-      errors = 0
-      for xi, target in zip(X, y):
-        update = self.eta * (target - self.predict(xi))
-        self.w_[1:] += update * xi
-        self.w_[0] += update * xi
-        errors += int(update != 0.0)
-      self.erros_.append(errors)
-    
-  def predict(self, X):
-    return np.where(self.net_input(X) >= 0.0, 1, -1)
-  def net_input(self, X):
-    return np.dot(X, self.w_[1:]) + self.w_[0]
-  
+    def train(self, training_data, epochs, learning_rate):
+        for _ in range(epochs):
+            for inputs, target in training_data:
+                prediction = self.predict(inputs)
+                error = target - prediction
+                for i in range(len(self.weights)):
+                    self.weights[i] += learning_rate * error * inputs[i]
+                self.bias += learning_rate * error
+
+def train_perceptron_for_logic_gate(gate, n):
+    if gate == "AND":
+        training_data = [(inputs, int(all(inputs))) for inputs in (zip(*[[0, 1] for _ in range(n)]))]
+    elif gate == "OR":
+        training_data = [(inputs, int(any(inputs))) for inputs in (zip(*[[0, 1] for _ in range(n)]))]
+
+    perceptron = Perceptron(n)
+    perceptron.train(training_data, epochs=1000, learning_rate=0.1)
+
+    return perceptron
+
+def test_perceptron(perceptron, n):
+    test_data = [(inputs, int(all(inputs))) for inputs in (zip(*[[0, 1] for _ in range(n)]))]
+    for inputs, target in test_data:
+        prediction = perceptron.predict(inputs)
+        print(f"Entrada: {inputs} => Saída: {prediction} (Esperado: {target})")
+
+n = input("Insert number of entries: ")  # Número de entradas
+and_perceptron = train_perceptron_for_logic_gate("AND", n)
+print("Perceptron para a função AND:")
+test_perceptron(and_perceptron, n)
+
+or_perceptron = train_perceptron_for_logic_gate("OR", n)
+print("Perceptron para a função OR:")
+test_perceptron(or_perceptron, n)
+
+xor_test_data = [(inputs, int(inputs[0] ^ inputs[1])) for inputs in [(0, 0), (0, 1), (1, 0), (1, 1)]]
+print("Perceptron para a função XOR:")
+for inputs, target in xor_test_data:
+    prediction = and_perceptron.predict(inputs)
+    print(f"Entrada: {inputs} => Saída: {prediction} (Esperado: {target})")
